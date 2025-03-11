3af76e5b8ae4d1f0659beff6a29db2403afd4d9b
unknown <huyen.le@infis.cz>
2025-03-11 08:35:42 +0100

    prvni

---
+++ poznamky.py
@@ -0,0 +1,24 @@
+#nacitani od uzivatele
+cislo=input
+
+#pretypovani
+cislo=int(input("Zadej cislo"))
+
+#podminky
+if cislo<10 and cislo==7:
+    print("cislo je mensi netz 10 a je 7")
+elif cislo>10:
+    print("cislo je vetsi nez 10")
+else:
+    print("cislo je 10")
+
+#logicke operatory
+
+#vypsat
+
+#github prikazy
+#1 git add *
+#2 git commit -m "prvni"
+#3 git push
+
+#hnananananannanananananananananananananananannanananananannanananananananananananananananananananananannanananananananannanananananananannananananan
